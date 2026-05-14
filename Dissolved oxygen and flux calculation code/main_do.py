68654f44da0567a09398ff98e4b25c98d5ecc1a8
Frank Leon <78581682+Frank-Leon@users.noreply.github.com>
2026-05-14 19:26:02 +0800

    Update main_do.py

--- Dissolved oxygen and flux calculation code/main_do.py
+++ Dissolved oxygen and flux calculation code/main_do.py
@@ -11,12 +11,13 @@ column_num = [3, 8, 13]
 outcomes = []
 
 # 导出指定工况的三个点位的溶解氧数据
-with pd.ExcelWriter("溶解氧及通量结果导出1107(含hs).xlsx", mode="w") as writer:
+with pd.ExcelWriter("溶解氧及通量结果导出20260514(含hs).xlsx", mode="w") as writer:
+    "第一次mode=w，后面mode=a"
     for i in range(len(column_num)):
         # 从水动力结果.xlsx文件调取工况的1号点水力要素序列
         df = pd.read_excel("水动力结果导出.xlsx", sheet_name='工况1')
-        depths = df.iloc[0:61, column_num[i]].tolist()  # 获取第4列的数据
-        vels = df.iloc[0:61, column_num[i]+1].tolist()
+        depths = df.iloc[2:64, column_num[i]].tolist()  # 获取第4列的数据
+        vels = df.iloc[2:64, column_num[i]+1].tolist()
 
         # 1点位溶解氧变化及通量计算
         re_do_1 = ar.get_re_do_values(depths, vels)
@@ -34,35 +35,4 @@ with pd.ExcelWriter("溶解氧及通量结果导出1107(含hs).xlsx", mode="w") 
             "SWI通量": sw_fluxes_1,
          }
         outcomes.append(do_outcome_1)
-    write_sheet(writer, outcomes, node_indexs = list(range(1, 102, 10)), sheet_name = "1号点")
-
-
-
-# # 从水动力结果.xlsx文件调取工况3的1号点水力要素序列
-# df = pd.read_excel("水动力结果导出.xlsx", sheet_name='工况2')
-#
-# # 指定点位溶解氧变化计算
-# depths = df.iloc[1:62, 3].tolist()  # 获取第4列的数据
-# vels = df.iloc[1:62, 4].tolist()
-# do_values = ar.cal_do_values(depths, vels)
-#
-# # 沉积物水界面通量计算
-# sw_fluxes = sc.cal_sediment_water_flux(depths, vels, do_values, k=0.01033, c_ods=0.122, css=0.5)
-# # print(sw_fluxes)
-# # print(len(sw_fluxes))
-#
-# # 大气水界面通量
-# aw_fluxes = ar.cal_atmosphere_water_flux(depths, vels, do_values)
-# # print(aw_fluxes)
-# # print(len(aw_fluxes))
-
-# # 基于实测工况进行验证
-# path = "E:/_things/1组会/金汇港/溶解氧_2/结果/溶解氧计算output -S3.xlsx"
-# df = pd.read_excel(path, sheet_name="固定点复氧S3 (校正)")
-# depths = df.iloc[2:63, 1].tolist()  # 获取第4列的数据
-# vels = df.iloc[2:63, 2].tolist()
-#
-# print(cal_do_values(depths, vels))
-
-# #界面通量计算
-# 沉积物-水界面通量
+    write_sheet(writer, outcomes, node_indexs = [1, 2, 3], sheet_name = "工况1")
